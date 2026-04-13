f08ceeb3c9bd333cc7988e5434a12fbd46984561
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2026-04-13 20:45:25 -0300

    Setting up dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = 5000;
+const port = process.env.PORT;
 
-const app = express()
+const app = express();
 
 // config JSON and form data response
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
-    console.log(`App rodando na porta ${port}`)
+  console.log(`App rodando na porta ${port}`);
 });
