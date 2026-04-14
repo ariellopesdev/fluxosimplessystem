775a8d83882c8600c96e44d9df8e59b9557d0dba
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2026-04-13 21:41:08 -0300

    Product model created

---
+++ backend/models/Product.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const productSchema = new Schema(
+  {
+    name: String,
+    image: String,
+    quantity: Number,
+    price: Number,
+    description: String,
+    category: String,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
