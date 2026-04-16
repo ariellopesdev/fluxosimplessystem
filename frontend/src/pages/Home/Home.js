a5dd7828cde2ebf3a4f24a47f44a9414a215fb65
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2026-04-15 22:41:10 -0300

    Frontend of home started

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -8,7 +8,13 @@ const Home = () => {
   return (
     <div className="container-home">
       <div className="block-home">
-        <h1>Fluxo Simples System</h1>
+        <h1>
+          fluxo simples <span>system</span>
+        </h1>
+        <p>
+          Com o Fluxo Simples, você controla seu estoque de forma eficiente e
+          organiza agendamentos, prazos e entregas em um só lugar.
+        </p>
       </div>
       <form>
         <h2>Login</h2>
@@ -16,8 +22,27 @@ const Home = () => {
         <input type="email" />
         <label>Senha</label>
         <input type="password" />
+        <a>Esqueceu sua senha?</a>
         <button type="submit">Entrar</button>
+        <br />
+        <button type="">Criar uma conta</button>
       </form>
+      <footer>
+        <div>
+          <a>Cadastre-se</a>
+          <a>Entrar</a>
+        </div>
+        <div>
+          <a>Política de Privacidade</a>
+          <a>Central de Privacidade</a>
+          <a>Sobre</a>
+        </div>
+        <div>
+          <p>
+            &copy; <a>ariellopesdev</a> 2026. Todos os direitos reservados.
+          </p>
+        </div>
+      </footer>
     </div>
   );
 };
