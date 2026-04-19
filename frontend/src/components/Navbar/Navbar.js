5cba289046cb90ebf98c6ebc33370f3f3619e78d
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2026-04-19 12:53:42 -0300

    Navbar for authenticated users

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -8,23 +8,63 @@ import { NavLink, Link } from "react-router-dom";
 import { FaCircle } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
 
+//Hooks
+import { useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
 const Navbar = () => {
+  const { auth } = useAuth();
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <nav id="nav">
-      <header id="register__header">
-        <Link to="/">
-          <button className="register__header--company">
-            fluxo simples <span id="register__header--highlight">system</span>
-          </button>
-        </Link>
-        <form className="form__register--login">
-          <input type="email" placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
-          <button type="submit" className="register__btn--primary">
-            Entrar
-          </button>
-        </form>
-      </header>
+      <Link to="/">
+        <button className="nav--company">
+          fluxo simples <span className="nav--highlight">system</span>
+        </button>
+      </Link>
+      {auth ? (
+        <>
+          <ul className="nav__user--info">
+            <li
+              className="nav__user--iconLogged tooltip"
+              data-tooltip="Usuário online"
+            >
+              <FaCircle />
+            </li>
+            <li
+              className="nav__user--username tooltip"
+              data-tooltip="Nome do usuário"
+            >
+              Ariel Lopes
+            </li>
+            <li className="nav__user--id tooltip" data-tooltip="Código do usuário">
+              0049
+            </li>
+            <li
+              className="nav__user--companyName tooltip"
+              data-tooltip="Empresa"
+            >
+              Restaurante Rancho Alegre
+            </li>
+            <li className="nav__user--iconLogout tooltip" data-tooltip="Sair">
+              <FiLogOut />
+            </li>
+          </ul>
+        </>
+      ) : (
+        <>
+          <form className="nav__form--login">
+            <input type="email" placeholder="E-mail" />
+            <input type="password" placeholder="Senha" />
+            <button type="submit" className="register__btn--primary">
+              Entrar
+            </button>
+          </form>
+        </>
+      )}
     </nav>
   );
 };
