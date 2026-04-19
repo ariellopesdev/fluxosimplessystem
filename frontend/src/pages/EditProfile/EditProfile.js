5227d84a2f32b52b6d9910426b75020db89425cd
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2026-04-19 18:38:05 -0300

    Filling out the edit form with the user's data

--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -1,6 +1,47 @@
+//CSS
 import "./EditProfile.css";
 
+//Storage
+import { uploads } from "../../utils/config";
+
+//Hooks
+import { useState, useEffectEvent, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+//Redux
+import { profile, resetMessage } from "../../slices/userSlice";
+
+//Components
+import Message from "../../components/Message/Message";
+
 const EditProfile = () => {
+  const dispatch = useDispatch();
+
+  const { user, message, error, loading } = useSelector((state) => state.user);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [cnjp, setCnpj] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+  //Load user data
+  useEffect(() => {
+    dispatch(profile());
+  }, [dispatch]);
+
+  //Fill form with user data
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      setCompanyName(user.companyName);
+      setCnpj(user.cnpj);
+    }
+  }, [user]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -10,17 +51,37 @@ const EditProfile = () => {
       <p className="editProfile__subtitle">Adicione uma imagem de perfil.</p>
       {/* preview da imagem */}
       <form onSubmit={handleSubmit} className="editProfile__data">
-        <input type="text" placeholder="Nome completo" />
-        <input type="email" placeholder="Email" disabled />
-        <input type="text" placeholder="Nome da Empresa" disabled />
-        <input type="text" placeholder="CNPJ da Empresa" disabled />
+        <input
+          type="text"
+          placeholder="Nome completo"
+          onChange={(e) => setName(e.target.value)}
+          value={name || ""}
+        />
+        <input type="email" placeholder="Email" disabled value={email || ""} />
+        <input
+          type="text"
+          placeholder="Nome da Empresa"
+          disabled
+          value={companyName || ""}
+        />
+        <input
+          type="text"
+          placeholder="CNPJ da Empresa"
+          disabled
+          value={cnpj || ""}
+        />
         <label>
           <span>Imagem do Perfil:</span>
           <input type="file" />
         </label>
         <label>
           <span>Quer alterar sua senha?</span>
-          <input type="password" placeholder="Digite sua nova senha" />
+          <input
+            type="password"
+            placeholder="Digite sua nova senha"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password || ""}
+          />
         </label>
         <input type="submit" value="Atualizar dados" />
       </form>
