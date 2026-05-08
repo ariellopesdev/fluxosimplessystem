5ed8ee5fc89002b901e4ee533375bd50c0f1cfd5
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2026-05-08 19:34:57 -0300

    feat: create product model

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,25 +1,33 @@
-// const mongoose = require("mongoose");
-// const { Schema } = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-// const productSchema = new Schema(
-//   {
-//     name: String,
-//     description: String,
-//     price: Number,
-//     category: String,
-//     sku: String,
-//     stock: Number,
-//     image: String,
-//     company: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "Company",
-//     },
-//   },
-//   {
-//     timestamps: true,
-//   },
-// );
+const productSchema = new Schema(
+  {
+    name: String,
+    description: String,
+    price: Number,
+    productImage: String,
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company",
+    },
+    cnpj: String,
+  },
+  {
+    timestamps: true,
+  },
+);
 
-// const Product = mongoose.model("Product", productSchema);
+productSchema.index(
+  {
+    name: 1,
+    cnpj: 1,
+  },
+  {
+    unique: true,
+  },
+);
 
-// module.exports = Product;
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
