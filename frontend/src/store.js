68b2175cefb16138ac76c41c28bfbf11f689ea09
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2026-05-24 10:53:42 -0300

    feat: add client reducer and service integration

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import userReducer from "./slices/userSlice";
 import productReducer from "./slices/productSlice";
+import clientReducer from "./slices/clientSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
     product: productReducer,
+    client: clientReducer,
   },
 });
